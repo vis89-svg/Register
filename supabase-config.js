e3b194cc7708cfb1acf15ce8070ed49da9b74553
Vishag S <76469588+vis89-svg@users.noreply.github.com>
2026-07-15 21:21:32 +0530

    Supbase confid & routes

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://hirgkfhklwtbiajpxhaw.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpcmdrZmhrbHd0YmlhanB4aGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMzg3NzUsImV4cCI6MjA5OTYxNDc3NX0.mrHTOZIwnFG41FhZSsr8pRWH7-cCXfEH3ILj-Sh3U1s';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: { storage: window.sessionStorage }
+});
 export const auth = supabase.auth;
