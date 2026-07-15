698999b399754dafe1143396876652c99b4b9421
Vishag S <76469588+vis89-svg@users.noreply.github.com>
2026-07-15 17:58:55 +0530

    auth guard added @ Display

--- supabase-config.js
+++ supabase-config.js
@@ -4,3 +4,4 @@ const supabaseUrl = 'https://hirgkfhklwtbiajpxhaw.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpcmdrZmhrbHd0YmlhanB4aGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMzg3NzUsImV4cCI6MjA5OTYxNDc3NX0.mrHTOZIwnFG41FhZSsr8pRWH7-cCXfEH3ILj-Sh3U1s';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const auth = supabase.auth;
